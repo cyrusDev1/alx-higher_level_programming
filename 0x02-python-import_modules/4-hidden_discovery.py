bc179f1e93b41fabcaa276b69b554a7c4f9d14e7
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-02 16:47:55 +0100

    hidden discovery

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from hidden_4 import *
+    lists = dir()
+    for i in range(len(lists)):
+        if lists[i][:2] != '__':
+            print("{}".format(lists[i]))
