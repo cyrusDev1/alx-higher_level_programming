6d8c65c39ea3a61c322042412e4d3ef5b805af47
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-02 23:25:52 +0100

    bytecode

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -5,6 +5,6 @@ def magic_caculation(a, b):
         c = add(a, b)
         for i in range(4, 6):
             c = add(c, i)
-        return c
+        return (c)
     else:
-        return sub(a, b)
+        return (sub(a, b))
