8d4a98a40f4be5663bc7870eab6e948ab7c24f82
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-02 15:23:16 +0100

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    lens = len(sys.argv) - 1
+    if lens == 0:
+        print("{} arguments.".format(lens))
+    else:
+        print("{} arguments:".format(lens))
+        for i in range(1, lens+1):
+            print("{}: {}".format(i, sys.argv[i]))
