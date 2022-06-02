b55b2f14804543adf5d1322a18468e241d90cb1e
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-02 15:25:39 +0100

    args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,7 +4,9 @@ if __name__ == "__main__":
     lens = len(sys.argv) - 1
     if lens == 0:
         print("{} arguments.".format(lens))
+    elif lens == 1:
+        print("{} argument.".format(lens))
     else:
         print("{} arguments:".format(lens))
-        for i in range(1, lens+1):
+    for i in range(1, lens+1):
             print("{}: {}".format(i, sys.argv[i]))
