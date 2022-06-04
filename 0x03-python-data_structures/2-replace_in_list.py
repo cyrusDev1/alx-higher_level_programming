3b0b63fd906f3db78b10cce8d145a62548e97c1b
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-04 10:29:33 +0100

    replace in list

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
+def replace_in_list(my_list, idx, element):
     lens = len(my_list)
     if idx < 0 or idx >= lens:
         return None
-    elm = my_list[idx]
-    return elm
+    my_list[idx] = element
+    return my_list
