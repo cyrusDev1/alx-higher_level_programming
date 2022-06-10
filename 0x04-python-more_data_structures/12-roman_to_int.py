ae49fccf5d9c0c07c55711fcd90ba045ef4884c1
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-11 00:19:03 +0100

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    if not isinstance(roman_string, str) or roman_string == None:
+        return 0
+    ret = 0
+    two = {'CM': 800, 'CD': 400, 'XC': 90, 'XL': 40, 'IX': 9, 'IV': 4}
+    one = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    i = 0
+    while i < len(roman_string):
+        if i < len(roman_string) - 1 and roman_string[i:i+2] in two:
+            ret += two[roman_string[i:i+2]]
+            i += 2
+        else:
+            ret += one[roman_string[i]]
+            i += 1
+    return ret
