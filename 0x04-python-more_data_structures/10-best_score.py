7df36c3aa9ac664c230937e4edd2b93418cd5169
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-10 18:58:48 +0100

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,7 +3,7 @@ def best_score(a_dictionary):
     if isinstance(a_dictionary, dict) and len(a_dictionary) > 0:
         all_key = list(a_dictionary.keys())
         key = all_key[0]
-        for k , v in a_dictionary.items():
+        for k, v in a_dictionary.items():
             if v > a_dictionary[key]:
                 key = k
         return key
