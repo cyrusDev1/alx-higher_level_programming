1af7a6575c7ef92ff0c66eb88457954d64c89ed7
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-27 13:51:01 +0100

    3-rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -51,6 +51,7 @@ class Rectangle():
         return ((self.__width + self.__height) * 2)
 
     def __str__(self):
+        """Represents the rectangle with the character #"""
         rectangle = ""
         if self.__width == 0 or self.__height == 0:
             return rectangle
