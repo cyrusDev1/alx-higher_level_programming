ab39c79a84ed97df9a3abfcbdc4daa6d33000831
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-27 13:22:01 +0100

    3 rectangle

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -55,5 +55,6 @@ class Rectangle():
         for h in range(self.__height):
             for w in range(self.__width):
                 rectangle += "#"
-            rectangle += "\n"
+            if h != self.__height - 1:
+                rectangle += "\n"
         return rectangle
