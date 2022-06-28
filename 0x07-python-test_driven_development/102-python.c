ca6939a58250ed7878104e5e0a0fe5efea1fad16
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-28 23:06:57 +0100

    cpython

---
+++ 0x07-python-test_driven_development/102-python.c
@@ -0,0 +1,27 @@
+#include <Python.h>
+
+void print_python_string(PyObject *p)
+{
+    const char *type = NULL;
+    Py_ssize_t len = 0;
+
+    printf("[.] string object info\n");
+    if (!PyUnicode_Check(p))
+    {
+        printf("  [ERROR] Invalid String Object");
+        return;
+    }
+
+    if (PyUnicode_IS_COMPACT_ASCII(p))
+    {
+        type = "compact ascii";
+    }
+    else
+    {
+        type = "compact unicode object";
+    }
+
+    printf("  type: %s\n", type);
+    printf("  length: %ld\n", len);
+    printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &len));
+}
