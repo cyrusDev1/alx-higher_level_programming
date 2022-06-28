fc231bf2717aab3f5a16c1b226a58b95fd39be4a
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-28 22:23:45 +0100

    task 3

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Defines function that say may name"""
+
+
 def say_my_name(first_name, last_name=""):
+    """Say may name"""
     if not isinstance(first_name, str):
         raise TypeError("first_name must be a string")
     if not isinstance(last_name, str):
