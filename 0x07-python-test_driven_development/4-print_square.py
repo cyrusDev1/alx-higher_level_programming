8f39a1e33243b594d082b0ac54bc9bb3cf9f51c8
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-06-28 22:26:06 +0100

    task 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Defines function that print a square"""
+
+
 def print_square(size):
+    """Prints a square"""
     if isinstance(size, float) and size < 0:
         raise TypeError("size must be an integer")
     if not isinstance(size, int):
