e25b743beb2207c39ebbced68cb418d40af332c1
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-07-04 16:36:31 +0100

    task 9

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -19,4 +19,4 @@ class Rectangle(BaseGeometry):
 
     def __str__(self):
         """Returns description of obj"""
-        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+        return ("[{}] {}/{}".format(self.__class__.__name__,self.__width, self.__height))
