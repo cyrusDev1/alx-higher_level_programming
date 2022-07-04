197dbb515588735caf8bc6f5a3e3f7ed9805dac5
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-07-04 17:52:48 +0100

    task 100

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,17 @@
+from distutils.command.clean import clean
+
+
+#!/usr/bin/python3
+"""Defines class MyInt"""
+
+
+class MyInt(int):
+    """Reverse == and !="""
+
+    def __eq__(self, other):
+        """Reverse =="""
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """Reverse !="""
+        return super().__eq__(other)
