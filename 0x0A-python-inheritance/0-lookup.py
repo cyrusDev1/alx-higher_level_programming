512392be7b6c52670f7e5472e8eca2542f364354
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-07-04 12:56:27 +0100

    task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Defines lookup function"""
+
+
+def lookup(obj):
+    """Returns the list of available attributes
+    and methods of an object
+    """
+    return (dir(obj))
