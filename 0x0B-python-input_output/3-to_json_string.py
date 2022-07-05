e90701e81ff3b9ea4bf1b7fbc9787f09c8fcbc40
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-07-05 14:41:28 +0100

    task 4

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines to_json_string function"""
+
+
+import json
+
+
+def to_json_string(my_obj):
+    """Returns the JSON representation of an object (string):
+
+    Args:
+        my_obj (str): object
+    """
+    return json.dumps(my_obj)
