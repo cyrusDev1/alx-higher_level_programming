f7616faa7031ca051c8597da97fe54399c56d153
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-07-05 13:05:51 +0100

    task 0 && 1

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Defines read_file function"""
+
+
+def read_file(filename=""):
+    """Reads a file and prints it to output"""
+    with open(filename, encoding="utf-8") as f:
+        for line in f:
+            print(line, end="")
+        print()
