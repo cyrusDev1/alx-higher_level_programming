d0a21859ee135589d0eec252e1df0b6e9d25e8bf
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-05 18:55:55 +0100

    square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const argv = process.argv;
+const parse = parseInt(argv[2], 10);
+if (isNaN(parse)) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < parse; i++) {
+    console.log("X".repeat(parse));
+  }
+}
