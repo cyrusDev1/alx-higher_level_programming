3faa4b134b61aa8b51f98dbd9adba0e019c90472
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-05 19:23:24 +0100

    factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const argv = process.argv;
+const parse = parseInt(argv[2], 10);
+
+function factorial (n) {
+  if (n == 1) {
+    return (1);
+  }
+  return (n * factorial(n - 1));
+}
+
+if (isNaN(parse)) {
+  console.log('1');
+} else {
+  const fact = factorial(parse);
+  console.log(f);
+}
