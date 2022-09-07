3ddf89477b9aebbf41fab10b7322f4d1f8950437
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-07 20:06:34 +0100

    task 13 concat files

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 const argv = process.argv;
 
-const textA = fs.readFileSync(argv[2], 'utf-8', function (err) { if (err) { console.log(err); } });
-const textB = fs.readFileSync(argv[3], 'utf-8', function (err) { if (err) { console.log(err); } });
+const textA = fs.readFileSync(argv[2], 'utf8', function (err) { if (err) { console.log(err); } });
+const textB = fs.readFileSync(argv[3], 'utf8', function (err) { if (err) { console.log(err); } });
 const textC = textA.concat(textB);
-fs.writeFile(argv[4], textC, 'utf-8', function (err) { if (err) { console.log(err); } });
+fs.writeFile(argv[4], textC, 'utf8', function (err) { if (err) { console.log(err); } });
 console.log(textB);
