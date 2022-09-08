9e879e2d9d88a3a439da04ecf3ba850f278a24bb
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-08 16:44:55 +0100

    update

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,18 +3,20 @@
 import sys
 import MySQLdb
 
-argv = sys.argv
-host = "localhost"
-port = 3306
-user = argv[1]
-passwd = argv[2]
-db_name = argv[3]
 
-db = MySQLdb.connect(
-    host=host, port=port, user=user, passwd=passwd, db=db_name
-    )
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+if __name__ == "__main__":
+    argv = sys.argv
+    host = "localhost"
+    port = 3306
+    user = argv[1]
+    passwd = argv[2]
+    db_name = argv[3]
+
+    db = MySQLdb.connect(
+        host=host, port=port, user=user, passwd=passwd, db=db_name
+        )
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,19 +4,21 @@
 import sys
 import MySQLdb
 
-argv = sys.argv
-host = "localhost"
-port = 3306
-user = argv[1]
-passwd = argv[2]
-db_name = argv[3]
+if __name__ == "__main__":
+    argv = sys.argv
+    host = "localhost"
+    port = 3306
+    user = argv[1]
+    passwd = argv[2]
+    db_name = argv[3]
 
-db = MySQLdb.connect(
-    host=host, port=port, user=user, passwd=passwd, db=db_name
-    )
-cursor = db.cursor()
-cursor.execute(
-    "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY states.id ASC")
-rows = cursor.fetchall()
-for row in rows:
-    print(row)
+    db = MySQLdb.connect(
+        host=host, port=port, user=user, passwd=passwd, db=db_name
+        )
+    cursor = db.cursor()
+    cursor.execute(
+        """SELECT * FROM states WHERE name
+        LIKE BINARY 'N%' ORDER BY states.id ASC""")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
