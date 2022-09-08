cca6afa741296fd7f58ef5482abf013e5b0d451c
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-08 19:30:21 +0100

    task 3 sql injection

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument. But this time,
+write one that is safe from MySQL injections!"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    host = "localhost"
+    port = 3306
+    user = argv[1]
+    passwd = argv[2]
+    db_name = argv[3]
+    enter = argv[4]
+    db = MySQLdb.connect(
+        host=host, port=port, user=user, passwd=passwd, db=db_name
+        )
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states WHERE name = %s
+                ORDER BY states.id ASC""", (enter,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
