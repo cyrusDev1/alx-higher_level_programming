5b9c25da01817814d2a3115573bd80722512e320
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-09 11:33:02 +0100

    task 4 cities by states

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""takes in arguments and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument. But this time,
+write one that is safe from MySQL injections!"""
+import sys
+import MySQLdb
+
+
+if __name__ == "__main__":
+    argv = sys.argv
+    host = "localhost"
+    port = 3306
+    user = argv[1]
+    passwd = argv[2]
+    db_name = argv[3]
+    db = MySQLdb.connect(
+        host=host, port=port, user=user, passwd=passwd, db=db_name
+        )
+    cursor = db.cursor()
+    cursor.execute("""SELECT cities.id, cities.name, states.name FROM
+    hbtn_0e_4_usa.cities INNER JOIN hbtn_0e_4_usa.states ON
+    cities.state_id = states.id ORDER BY states.id ASC""")
+
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
