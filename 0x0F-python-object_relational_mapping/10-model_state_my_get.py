94d5827effa87adf72740ac5879671cb7b574ac0
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-10 12:34:13 +0100

    task 10 model state

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -12,9 +12,9 @@ if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
         sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
     session = Session(bind=engine)
-    rows = session.query(State).filter(State.name == sys.argv[4])
-    if rows[0]:
-        print(f'{rows[0].id}')
+    row = session.query(State).filter(State.name == sys.argv[4]).first()
+    if row:
+        print(f'{row.id}')
     else:
         print("Not found")
     session.close()
