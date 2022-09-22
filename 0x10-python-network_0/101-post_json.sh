9475aa10c1cf6caf386f0da6d41fac84373f61a0
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-22 22:20:33 +0100

    post json

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
+# sends a JSON POST request to a URL
 curl -s --json @"$2" "$1"
