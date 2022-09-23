b4ff7f8186f7d592c9714ec6730e739a48f433e4
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-23 11:32:43 +0100

    catch me

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # makes a request to 0.0.0.0:5000/catch_me that
-curl -sX PUT -L -d "user_id=98" 0:5000/catch_me -H "Origin: You got me!"
+curl -sX PUT -L -d "user_id=98" 0:5000/catch_me -H "Origin: HolbertonSchool"
