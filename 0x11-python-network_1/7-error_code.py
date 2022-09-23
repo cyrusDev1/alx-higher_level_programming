c7cadd9e2ac1dce3a2db7897c7f0c50b290fef5a
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-23 21:11:10 +0100

    error code request

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a Python script that takes in a URL, sends a request to the URL
+and displays the body of the response (decoded in utf-8)."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    response = requests.get(url)
+    code = response.status_code
+    if (code >= 400):
+        print("Error code: {}".format(code))
+    else:
+        print(response.text)
