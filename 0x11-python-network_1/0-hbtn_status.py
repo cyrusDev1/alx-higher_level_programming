df6c682eab637effe9a9259c73308b8adf99ea45
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-23 15:58:39 +0100

    task 0 What's my status update

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
     req = urllib.request.Request(url)
     with urllib.request.urlopen(req) as response:
         content = response.read()
-        print("Body response:\n\t- type: {}\n\
-        - content: {}\n\t- utf8 content: {}".format(
-            type(content), content, content.decode("utf8")))
+        print("Body response:")
+        print("\t- type: {}".format(type(content)))
+        print("\t- content: {}".format(content))
+        print("\t- utf8 content: {}".format(content.decode("utf8")))
