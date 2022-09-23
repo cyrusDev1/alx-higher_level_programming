38076bb81c2f7803252c04cf9d53d7b668ee4c91
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-23 22:39:57 +0100

    task 8 json api

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""A Python script that takes in a letter and sends a POST request
+to http://0.0.0.0:5000/search_user with the letter as a parameter."""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = "http://0.0.0.0:5000/search_user"
+    q = "" if len(sys.argv) == 1 else sys.argv[1]
+    response = requests.post(url, {'q': q})
+    json = eval(response.text)
+    if json == {}:
+        print("No result")
+    else:
+        try:
+            print("[{}] {}".format(json.get("id"), json.get("name")))
+        except Exception:
+            print("Not a valid JSON")
