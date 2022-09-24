12ec099141a7d5c8daf2094f892bb41c7ba44d0c
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-24 12:00:41 +0100

    github commit

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""list 10 commits (from the most recent to oldest) of
+the repository"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    repo = sys.argv[1]
+    name = sys.argv[2]
+    url = "https://api.github.com/repos/" + name + '/' + repo + "/commits"
+    response = requests.get(url)
+    commits = response.json()
+    for i in range(0, 10):
+        sha = commits[i].get('sha')
+        name = commits[i].get('commit').get('author').get('name')
+        print("{} {}".format(sha, name))
