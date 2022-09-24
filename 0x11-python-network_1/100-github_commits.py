fa7d85f61145dc23d848cb42b511ab3f91b08873
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-09-24 12:17:30 +0100

    update github commit

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -11,7 +11,10 @@ if __name__ == "__main__":
     url = "https://api.github.com/repos/" + name + '/' + repo + "/commits"
     response = requests.get(url)
     commits = response.json()
-    for i in range(10):
-        sha = commits[i].get('sha')
-        name = commits[i].get('commit').get('committer').get('name')
-        print("{}: {}".format(sha, name))
+    try:
+        for i in range(0, 10):
+            sha = commits[i].get('sha')
+            name = commits[i].get('commit').get('author').get('name')
+            print("{} {}".format(sha, name))
+    except Exception:
+        pass
