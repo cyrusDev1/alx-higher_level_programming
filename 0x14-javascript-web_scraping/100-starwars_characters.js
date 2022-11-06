7b38c743b66eab7c962b1def10571e5efc0cecc3
cyrusDev1 <cyrushoungue1240@gmail.com>
2022-11-06 13:02:22 +0100

    task 100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,22 @@
+#!/usr/bin/node
+const request = require('request');
+const argv = process.argv;
+const url = 'https://swapi-api.hbtn.io/api/films/';
+const id = argv[2];
+const endpoint = `${url}${id}`;
+
+request(endpoint, (err, response, body) => {
+  if (!err) {
+    const data = JSON.parse(body);
+    characters = data.characters;
+    characters.forEach(url => {
+      request(url, (err, response, body) => {
+        if (!err) {
+          const data = JSON.parse(body);
+          const name = data.name;
+          console.log(name);
+        }
+      });
+    });
+  }
+});
